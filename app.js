1780178588a4c21c524b44d844c4bed3a5154c3c
João <programador09@speedgov.com.br>
2022-05-12 21:41:19 -0300

    criando todas as rotasdoc rud

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ import express from "express";
 
 const router = express.Router();
 const app = express();
+const bodyParser = require('body-parser')
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("<html><head><meta name='viewport' content='width=device-'> <body> <h1>BEM VINDO</h1></body></html>")
+})
+
+app.get("/lendas-urbanas/:index", (req, res) => {
+    res.send(lendas[req.params.index]);
+})
+
+app.post("/lendas-urbanas/", (req, res) => {
+    lendas.push(req.body);
+    res.send({message: "Criado com Sucesso"});
+})
+
+app.put("/lendas-urbanas/:index", (req, res) => {
+    lendas[req.params.index] = req.body;
+    res.send({message: "Atualizado com Sucesso"});
+})
+
+app.delete("/lendas-urbanas/:index", (req, res) => {
+    lendas.splice(req.params.index, 1);
+    res.send({message: "Deletado com Sucesso"});
 })
 
 app.listen(3000, function() {
