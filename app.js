878f2f1562e63fdc8baed74d85cd43e8a7d26fae
Your Name <you@example.com>
2022-05-12 21:42:07 -0300

    criando collection das lendas

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ import express from "express";
 
 const router = express.Router();
 const app = express();
+const lendas = [
+    {
+        nome: "Mula sem Cabeça"
+    },
+    {
+        nome: "Curupira",
+        criador: ""
+    }
+]
 
 app.use(express.json());
+
+app.get("/lendas-urbanas", (req,res) => {
+    res.json(lendas)
+})
 
 app.get('/', (req, res) => {
     res.send("<html><head><meta name='viewport' content='width=device-'> <body> <h1>BEM VINDO</h1></body></html>")
